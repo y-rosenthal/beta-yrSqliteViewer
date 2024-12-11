de54c4ce481417697368056e0506b84fa8372757
YR <yrosenthal@gmail.com>
2024-12-11 09:47:28 -0500

    added main.js, tableManager.js - right now they just contain stubs and aren't used in the actual codebase

---
+++ tableManager.js
@@ -0,0 +1,9 @@
+function createTableCardHTML(tableName, columns) {
+    // ... existing code ...
+}
+
+function addTableCard(tableName, columns) {
+    // ... existing code ...
+}
+
+export { createTableCardHTML, addTableCard };
